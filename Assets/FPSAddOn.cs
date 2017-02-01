be9d777c8884ea57b777b56c1d162409da70f04e
B. Ricks <bricks@unomaha.edu>
2017-02-01 11:47:08 -0600

    Starting basic FPS seed.

---
+++ Assets/FPSAddOn.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FPSAddOn : MonoBehaviour
+{
+
+    public GameObject bullet;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetMouseButtonDown(0))
+	    {
+	        Fire();
+	    }
+
+
+
+    }
+
+    private void Fire()
+    {
+
+        Camera characterCamera = this.gameObject.GetComponentInChildren<Camera>();
+
+        Transform bulletRef = (Transform)Instantiate(bullet, characterCamera.transform.position + characterCamera.transform.forward, new Quaternion());
+        bulletRef.gameObject.GetComponent<Rigidbody>().AddForce( characterCamera.transform.forward * 1000);
+
+
+        Destroy(bulletRef.gameObject, 2);
+
+    }
+}
